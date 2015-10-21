5abb2441e010a52d5b1515d3b5b26197310dd50d
aergawy <amr.ergawy@gmail.com>
2015-10-21 12:23:17 +0200

    - Trying to enforce sauqre cells as computed-size divs instead of using the border-spacing property of the table.

--- script/board.js
+++ script/board.js
@@ -1,17 +1,28 @@
-$(document).ready(
-	function() {
-		var rows = 16;
-		var cols = 2*rows;
-		
-		var table = document.getElementById("board");
-		
-		for (i = 0; i < rows; i++) {
-			var row = table.insertRow(i);
-			for (j = 0; j < cols; j++) {
-				var cell =  row.insertCell(j);
-				// cell.innerHTML = "NEW CELL";
-				cell.className = "board-cell";
-			}
-		}
+$(document).ready(function() {
+	console.log('handling document ready ...');
+
+	boardLayout(16, 16);
+
+	console.log('handled document ready')
+});
+
+var boardLayout = function(rows, cols)
+{
+	console.log('starting board layout with (rows, cols) = ('+rows+', '+cols+')');
+
+	if (rows < 2 || cols < 2) {
+		alert('board must be at least 2x2, please re-select its size!');
+		console.log('wrong board size, exiting board layout');
+		return;
 	}
-);
+
+	var bf = $('#board-frame');
+	console.log('board-frame: '+bf.width()+'x'+bf.height());
+
+	var cellWidth = Math.floor(bf.width()/cols);
+	var cellHeight = Math.floor(bf.height()/rows);
+	cellWidth > cellHeight ? cellWidth = cellHeight : cellHeight = cellWidth;
+	console.log('square-cell: '+cellWidth+'x'+cellHeight);
+
+	console.log('done with board layout')
+}
